c90922a24a802a9191dec8af96854bf3d084671d
Copynightmare <43727591+copycat666@users.noreply.github.com>
2024-03-07 17:32:00 +0100

    Projektdateien hinzufügen.

---
+++ InfiniteSprint/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,27 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace InfiniteSprint.Patches
+{
+    [HarmonyPatch(typeof(GameNetcodeStuff.PlayerControllerB))]
+    internal class PlayerControllerPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        private static void Postfix(GameNetcodeStuff.PlayerControllerB __instance)
+        {
+            if (__instance.isSprinting)
+            {
+                if (!__instance.isInsideFactory)
+                    __instance.sprintMeter = 1f;
+                } 
+            }
+        }
+    }
+
---
+++ InfiniteSprint/Plugin.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using GameNetcodeStuff;
+using HarmonyLib;
+using InfiniteSprint.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+namespace InfiniteSprint
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class InfiniteSprintBase : BaseUnityPlugin
+    {
+        private const string modGUID = "CopyCat666.InfiniteSprint";
+
+        private const string modName = "Infinite Sprint";
+
+        private const string modVersion = "1.0.1";
+
+        private readonly Harmony harmony = new Harmony("CopyCat666.InfiniteSprint");
+
+        private static InfiniteSprintBase instance;
+       
+        internal ManualLogSource mls;
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            mls = base.Logger;
+            mls.LogInfo((object)"NaNSprint active!");
+            harmony.PatchAll();
+        }
+    }
+}
